c2bc0b3569a7aea18484d5c13351226925f07a56
Brian Mullen <bmullen@molbie.co>
2018-05-26 15:01:19 -0600

    Add source files

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+
+let package = Package(
+	name: "libpq",
+	pkgConfig: "libpq",
+	providers: [
+        .brew("postgres"),
+        .apt("libpq-dev"),
+	]
+)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # swift-libpq-macos-installer
-C module import for libpq on macOS using the PostgreSQL installer
+
+C module import for libpq on macOS using the PostgreSQL installer:
+https://www.postgresql.org/download/macosx/
+
+The files in this repository are required for the Swift Package Manager.
